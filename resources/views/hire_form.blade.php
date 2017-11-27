e73fec76e01258a7562f4fe7f1769bf457890363
Sunday Ugorji <sundayugoji4@gmail.com>
2017-11-27 22:44:16 +0100

    Removed the border

--- resources/views/hire_form.blade.php
+++ resources/views/hire_form.blade.php
@@ -1,54 +1,40 @@
 @extends('layouts.default')
 @section('content')
 
-    <?php
-    if (isset($msg)) {
-        echo '</br></br></br></br></br><div id="formmessage"><p>', $msg , '<a href="index.php"> Back to home page</a></p></div>';
-    }else {
-    ?>
-    </br></br></br></br></br></br>
-    <div class="form-group container" >
-        <form id="myform" name="theform" class="group" action="<?php $_SERVER['PHP_SELF']; ?>" method="POST">
-            <span id="formerror" class="error"></span>
+       <div class="container">  
+          <form id="contact" action="" method="post">
 
-            <label for="myname">Name *</label>
-            <input type="text" class="form-control" name="myname" id="myname" required title="Please enter your name" autofocus placeholder="" value="<?php if (isset($myname)) { echo $myname; } ?>" />
-            <?php if (isset($err_myname)) { echo $err_myname; } ?>
+                <h3>Quick Contact</h3>
+                
+                <h4>Contact us today, and get reply with in 24 hours!</h4>
+                
+                <fieldset>
+                  <input placeholder="Your name" type="text" tabindex="1" required autofocus>
+                </fieldset>
 
-            <label for="myemail">Email Address *</label>
-            <input type="email" class="form-control" name="myemail" id="myemail" title="Please enter your Email" autofocus  value="<?php if (isset($myemail)) { echo $myemail; } ?>" />
-            <?php if (isset($err_myemail)) { echo $err_myemail; } ?>
+                <fieldset>
+                  <input placeholder="Your Email Address" type="email" tabindex="2" required>
+                </fieldset>
 
-            <label for="myphone_number">Phone Number *</label>
-            <input type="tel" class="form-control" name="myphone_number" id="myphone_number" title="Please enter your phone number" autofocus value="<?php if (isset($myphone_number)) { echo $myphone_number; } ?>" />
-            <?php if (isset($err_myphone_number)) { echo $err_myphone_number; } ?>
+                <fieldset>
+                  <input placeholder="Your Phone Number" type="tel" tabindex="3" required>
+                </fieldset>
 
-            <label for="mycountry">Country *</label>
-            <input type="text" class="form-control" name="mycountry" id="mycountry" title="Please enter your country" autofocus value="<?php if (isset($mycountry)) { echo $mycountry; } ?>" />
-            <?php if (isset($err_mycountry)) { echo $err_mycountry; } ?>
+                <fieldset>
+                  <input placeholder="Your Web Site starts with http://" type="url" tabindex="4" required>
+                </fieldset>
 
-            <label for="mycompanysite">Your company's website</label>
-            <input type="url" class="form-control" name="mycompanysite" id="mycompanysite" title="Please enter your name"  value="<?php if (isset($mycompanysite)) { echo $mycompanysite; } ?>" />
-            <?php if (isset($err_mycompanysite)) { echo $err_mycompanysite; } ?>
+                <fieldset>
+                  <textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
+                </fieldset>
 
-            <label for="reference">How did you hear about us?</label>
-            <select name="reference" id="reference" class="form-control">
-                <option value="">Choose...</option>
-                <option value="friend"
-                <?php if ((isset($reference)) && ($reference === 'friend')) { echo "selected"; } ?>>A friend</option>
-                <option value="facebook"
-                <?php if ((isset($reference)) && ($reference === 'facebook')) { echo "selected"; } ?>>Facebook</option>
-                <option value="twitter"
-                <?php if ((isset($reference)) && ($reference === 'twitter')) { echo "selected"; } ?>>Twitter</option>
-            </select>
+                <fieldset>
+                  <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+                </fieldset>
 
-            <label for="mycomments">Technical Need: * <br/> Tell us what you want our staff to do for you</label>
-            <textarea class="form-control" name="mycomments" id="mycomments" rows="10" ><?php if (isset($mycomments)) { echo $mycomments; } ?></textarea>
-
-            <button class="btn btn-primary btn-lg " type="submit" name="action" value="submit">send</button>
-        </form>
+          </form>
+     
+      
     </div>
-
-    <?php } ?>
 
 @stop
